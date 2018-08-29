8ea97d42ff73cb7067e19d0cafe35052342b6b31
eversC <chris.every@ovoenergy.com>
2018-08-29 11:17:54 +0100

    Add utils/client.go

---
+++ utils/client.go
@@ -0,0 +1,22 @@
+package utils
+
+import (
+	"log"
+
+	"k8s.io/helm/pkg/helm"
+)
+
+//Client creates a Helm client and checks the connection works
+func Client(local bool) (client *helm.Client) {
+	log.Println("Starting Helm client")
+	if local {
+		client = helm.NewClient(helm.Host("127.0.0.1:44134"))
+	} else {
+		client = helm.NewClient()
+	}
+	log.Println("Checking Helm client connection")
+	_, err := client.GetVersion()
+	PanicCheck(err)
+	log.Println("connection: OK")
+	return
+}
