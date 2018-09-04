66bfa66f70ac9db7094db23ef503ed6c27553ef8
eversC <chris.every@ovoenergy.com>
2018-09-04 20:11:45 +0100

    Add scripts/install.sh
    This will need (mostly) uncommenting when the plugin is
    opensourced, as the release file (binary/zip) will be puclicly
    available.

---
+++ scripts/install.sh
@@ -0,0 +1,45 @@
+#!/bin/sh
+
+version="$(cat plugin.yaml | grep "version" | cut -d '"' -f 2)"
+# cd $HELM_PLUGIN_DIR
+# version="$(cat plugin.yaml | grep "version" | cut -d '"' -f 2)"
+# echo "Installing helm-gcs ${version} ..."
+#
+# # Find correct archive name
+# unameOut="$(uname -s)"
+#
+# case "${unameOut}" in
+#     Linux*)     os=Linux;;
+#     Darwin*)    os=Darwin;;
+#     CYGWIN*)    os=Cygwin;;
+#     MINGW*)     os=windows;;
+#     *)          os="UNKNOWN:${unameOut}"
+# esac
+#
+# arch=`uname -m`
+# url="https://github.com/nouney/helm-gcs/releases/download/${version}/helm-gcs_${version}_${os}_${arch}.tar.gz"
+#
+# if [ "$url" = "" ]
+# then
+#     echo "Unsupported OS / architecture: ${os}_${arch}"
+#     exit 1
+# fi
+#
+# filename=`echo ${url} | sed -e "s/^.*\///g"`
+#
+# # Download archive
+# if [ -n $(command -v curl) ]
+# then
+#     curl -sSL -O $url
+# elif [ -n $(command -v wget) ]
+# then
+#     wget -q $url
+# else
+#     echo "Need curl or wget"
+#     exit -1
+# fi
+#
+# # Install bin
+# rm -rf bin && mkdir bin && tar xzvf $filename -C bin > /dev/null && rm -f $filename
+
+echo "helm-bulk ${version} is correctly installed."
