7d87d176c11a79c4672ad40c4631e166fbe69f66
eversC <chris.every@ovoenergy.com>
2019-01-02 14:04:50 +0000

    Remove superfluous helm-bulk suffix in change dir cmd

--- scripts/install.sh
+++ scripts/install.sh
@@ -5,7 +5,7 @@
  # You may obtain a copy of the License at
  #
  #     http://www.apache.org/licenses/LICENSE-2.0
- # 
+ #
  # Unless required by applicable law or agreed to in writing, software
  # distributed under the License is distributed on an "AS IS" BASIS,
  # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -15,7 +15,7 @@
 
 #copied almost verbatim from
 #https://github.com/nouney/helm-gcs/blob/master/scripts/install.sh
-cd $HELM_PLUGIN_DIR/helm-bulk
+cd $HELM_PLUGIN_DIR
 version="$(cat plugin.yaml | grep "version" | cut -d '"' -f 2)"
 echo "Installing helm-bulk v${version} ..."
 
